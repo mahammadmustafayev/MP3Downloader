d909c5366729b361d0f66d3e5ef09afd281629db
mmustafayev213-3 <mustafamehmed1251@gmail.com>
2024-01-30 16:00:14 +0400

    modified mp3 converter

--- MP3Converter/assets/app.js
+++ MP3Converter/assets/app.js
@@ -1,24 +1,26 @@
-const convertBtn= document.getElementById('convert-button'),
-      convertInput=document.getElementById('convert-input'),
-      resultTitle=document.querySelector('.result');
+const convertMobileBtn= document.getElementById('convert-button-mobile'),
+      convertMobileInput=document.getElementById('convert-input-mobile'),
+      resultMobileTitle=document.querySelector('.resultMobile');
+      
+      
 
-convertBtn.addEventListener('click',()=>{
+convertMobileBtn.addEventListener('click',()=>{
   
-  getAudio();
+  getMobileAudio();
 })
 
 
 
-async function getAudio(){
-    let link = convertInput.value;
-    let parts=link.split("=");
+async function getMobileAudio(){
+    let link = convertMobileInput.value;
+    //let parts=link.split("=");
     let partMobile=link.split('/')[3].split('?');
     let videoId="";
 
-    if (parts.length===2) {
-        videoId=parts[1];
-        console.log(videoId);
-    }
+    // if (parts.length===2) {
+    //     videoId=parts[1];
+    //     console.log(videoId);
+    // }
     if (partMobile.length===2) {
        videoId=partMobile[0];
        console.log(videoId);
@@ -36,21 +38,17 @@ async function getAudio(){
       },
     };
 
-    // const url =`https://youtube-video-download-info.p.rapidapi.com/dl?id=${videoId}`;
-    // const options={
-    //    method:'GET',
-    //    headers: {
-    //     'X-RapidAPI-Key': '26698c6f52msh5251d6fb37703e1p130839jsnde4bf25d0529',
-    //     'X-RapidAPI-Host': 'youtube-video-download-info.p.rapidapi.com',
-    //   },
-    // };
+    
 
     const response = await fetch(url,options);
     const result= await response.json();
-    resultTitle.innerHTML= `<p class="title">Title: ${result.title}</p>`;
+    resultMobileTitle.innerHTML= `<p class="title">Title: ${result.title}</p>`;
 
     setTimeout(() => {
         window.open(result.link,"_blank");
-        convertInput.value='';
+        convertMobileInput.value='';
     }, 1000);
 }
+
+
+
---
+++ MP3Converter/assets/main.js
@@ -0,0 +1,52 @@
+const convertWebBtn=document.getElementById('convert-button-web'),
+convertWebInput=document.getElementById('convert-input-web'),
+resultWebTitle=document.querySelector('.resultWeb');
+
+
+
+
+convertWebBtn.addEventListener('click',()=>{
+  
+    getWebAudio();
+  })
+  
+  
+  
+  async function getWebAudio(){
+      let link = convertWebInput.value;
+      let parts=link.split("=");
+      //let partMobile=link.split('/')[3].split('?');
+      let videoId="";
+  
+      if (parts.length===2) {
+          videoId=parts[1];
+          console.log(videoId);
+      }
+      // if (partMobile.length===2) {
+      //    videoId=partMobile[0];
+      //    console.log(videoId);
+      // }
+      else{
+        alert('Error Invalid Youtube Link!');
+      }
+  
+      const url =`https://youtube-mp36.p.rapidapi.com/dl?id=${videoId}`;
+      const options={
+         method:'GET',
+         headers: {
+          'X-RapidAPI-Key': '26698c6f52msh5251d6fb37703e1p130839jsnde4bf25d0529',
+          'X-RapidAPI-Host': 'youtube-mp36.p.rapidapi.com',
+        },
+      };
+  
+      
+  
+      const response = await fetch(url,options);
+      const result= await response.json();
+      resultWebTitle.innerHTML= `<p class="title">Title: ${result.title}</p>`;
+  
+      setTimeout(() => {
+          window.open(result.link,"_blank");
+          convertMobileInput.value='';
+      }, 1000);
+  }
